33f772888b4a653c36634d6e0ad5efd9fe60150f
Ojor-Immanuel <ojorimman@outlook.com>
2024-11-20 15:32:47 +0100

    First task for the day

---
+++ Tasks7.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Tasks7 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int x;
+        int y;
+
+        System.out.println("Enter first number: ");
+        x = input.nextInt();
+
+        System.out.println("Enter second number: ");
+        y = input.nextInt();
+
+        if (x > y)
+        System.out.println("X is larger than y");
+
+        if (x < y)
+        System.out.println("X is less than y");
+
+        if (x == y)
+        System.out.println("X is equal than y");
+
+
+
+
+
+
+    }
+}
